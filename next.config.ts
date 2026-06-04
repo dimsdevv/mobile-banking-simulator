6ec71af2d3e3c4b878ef292f48a671ba9c444130
dimsdevv <dimsdevv@users.noreply.github.com>
2026-06-04 16:18:06 +0700

    fix: next.config.ts errors

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,10 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
   turbopack: {},
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
